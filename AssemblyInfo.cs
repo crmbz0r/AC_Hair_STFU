f4b23ef205f15f5fdf508cc40fbaaf656359ebdc
crmbz0r <151in1hit@gmail.com>
2026-04-21 13:48:31 +0200

    Add AssemblyInfo and include in project
    Add a new AssemblyInfo.cs with assembly metadata (Title, Description, Company, Product, Copyright, Version). The description documents the plugin's debug/info log toggling to reduce console spam. Also update AC_Hair_STFU.csproj to compile AssemblyInfo.cs and include the new file in the build.

---
+++ AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("AC_Hair_STFU")]
+[assembly: AssemblyDescription(
+    "The AiComi Hair transparency plugin with added debug/info log toggling to reduce the unnecessary massive console log spam of this plugin."
+)]
+[assembly: AssemblyCompany("https://github.com/crmbz0r/AC_MakerAutoscroll")]
+[assembly: AssemblyProduct("AC_Hair_STFU")]
+[assembly: AssemblyCopyright("Copyright crmbz0r 2026")]
+[assembly: AssemblyVersion("0.0.2")]
+[assembly: AssemblyFileVersion("0.0.2")]
